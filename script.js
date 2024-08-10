73cc05830b391cfea19b8a1aa42e1cd274e48c43
BearerOP <ankity1892003@gmail.com>
2024-08-10 20:48:16 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// Redirection to zerodha Main Website on clicking the Zerodha Logo:
+document.getElementById("zerodha-navbar-logo").addEventListener("click" , () => {
+    window.open("https://zerodha.com/" , "_blank")
+})
+
+// Redirection to Github Repo on clicking the Menu Bar:
+document.getElementById("menu-bar-logo").addEventListener("click", () => {
+    window.location.href = "https://github.com/Shubhashish-Chakraborty/Zerodha-Landing-Page";
+});
+
+document.getElementById("signup-button").addEventListener("click", () => {
+    window.location.href = "https://github.com/Shubhashish-Chakraborty/Zerodha-Landing-Page";
+});
+
+document.querySelector("#redirectingImage").addEventListener("click" , () => {
+    window.open("https://zerodha.com/media" , "_blank")
+})
